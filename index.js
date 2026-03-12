1d498e23c1a2a5954f30ba1f704e5c3bdcd8adb7
SokhadomKhorn <tpcoea@gmail.com>
2026-03-12 16:04:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.get('/api/food', (req, res) => {
   res.json({ message: 'Welcome to FoodExpress API!', items: ['Pizza', 'Burger', 'Sushi'] });
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(5000, () =>
+    console.log('EXPRESS Server Started at Port No: 5000');
 });
 
 // POST API - Add new food item
