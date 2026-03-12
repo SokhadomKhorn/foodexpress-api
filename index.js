1bf8e6e38068a0966038951c89f3674183be39c1
SokhadomKhorn <tpcoea@gmail.com>
2026-03-12 16:12:07 +0700

    Refactor Express app and add new API routes
    Updated API endpoints and modified server initialization.

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
+const e1 = require('express');
+const app = e1();
 
-// GET API
-app.get('/api/food', (req, res) => {
-  res.json({ message: 'Welcome to FoodExpress API!', items: ['Pizza', 'Burger', 'Sushi'] });
+app.get('/search', (req, res) => {
+    res.send('<html><body>INSIDE SEARCH API..</body></html>');
 });
 
-app.listen(5000, () =>
+app.get('/view', (req, res) => {
+    res.send('<html><body>INSIDE VIEW API..</body></html>');
+});
+
+app.listen(5000, function() {
     console.log('EXPRESS Server Started at Port No: 5000');
-});
-
-// POST API - Add new food item
-app.post('/api/food', (req, res) => {
-  const { name } = req.body;
-  res.json({ message: `Food item '${name}' added successfully!` });
-});
-
-// DELETE API - Remove a food item
-app.delete('/api/food/:id', (req, res) => {
-  res.json({ message: `Food item ${req.params.id} deleted successfully!` });
 });
